8aac69b85da1fa50f0a8c038ce25259f53e03938
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-07-20 11:16:29 +0300

    Ending HW for junit 5

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,53 @@
+package ru.netology.service;
+
+import org.junit.Test;
+import org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void zeroCashBackNeed900Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 100;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void dealCashBackSayNoSay() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void zeroAmountSayNeedOneThousandAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void overLimitAmountSayNeed500Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void overLimitAmountSayNothing() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+}
