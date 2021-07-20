b219b552ae5297ae8fbdae3a8dfc706aaf4a7a7c
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-07-20 11:43:10 +0300

    Add adutotests 'TestNG'

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,54 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+
+public class CashbackHackServiceTest {
+    @Test
+    public void zeroCashBackNeed900Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 100;
+        int expected = 900;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void dealCashBackSayNoSay() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void zeroAmountSayNeedOneThousandAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void overLimitAmountSayNeed500Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void overLimitAmountSayNothing() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+}
